368234b11fd85f2ed8b28d7500948cb633bd652c
joshgarde <jgarde@Joshuas-Mac-Pro.local>
2014-03-25 22:24:17 -0700

    Basic Web Server System Established
    Thank you jetty. No need for licenses, proprietary software, or
    spaghetti code to link our front-end and backend.

--- src/com/styxcp/Styx.java
+++ src/com/styxcp/Styx.java
@@ -1,9 +1,16 @@
 package com.styxcp;
 
-import java.util.logging.*;
+import java.net.InetSocketAddress;
+
+import com.styxcp.web.server.FrontendServer;
 
 public class Styx {
 	public static void main(String [] args) {
 		System.out.println("Welcome to STYX EXTREMELY IN-DEV EDITION, This project is under the ownership of WoBHosting. WoBHosting Copyright 2014.");
+		System.out.println("Starting frontend web server...");
+		InetSocketAddress addr = InetSocketAddress.createUnresolved("127.0.0.1", 8880);
+		Runnable FrontendServer = new FrontendServer(addr);
+		new Thread(FrontendServer).start();
+		System.out.println("Frontend web server started on port 8880");
 	}
 }
--- src/com/styxcp/web/server/FrontendServer.java
+++ src/com/styxcp/web/server/FrontendServer.java
@@ -1,10 +1,25 @@
 package com.styxcp.web.server;
 
-public class FrontendServer {
-	public void start(int PortNUmber) {
-		
+import java.net.InetSocketAddress;
+
+import org.eclipse.jetty.server.Server;
+
+public class FrontendServer implements Runnable{
+	
+	private static InetSocketAddress addr;
+
+	public FrontendServer (InetSocketAddress address) {
+		addr = address;
 	}
-	public void stop() {
-		
+	
+	public void run(){
+		Server server = new Server(addr);
+        try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
